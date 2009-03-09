b7185b9f0a04d7734fbba20f898281c0135412c4
Matthew Todd <matthew.todd@gmail.com>
2009-03-09 10:37:48 +0300

    Properly load actual recipes from deprecation stub.

--- lib/wordpress/recipes/deploy.rb
+++ lib/wordpress/recipes/deploy.rb
@@ -3,13 +3,15 @@ puts 'DEPRECATION WARNING'
 puts '-' * 72
 puts 'wordpress/recipes/deploy will be removed in a future version.'
 puts 'Please adjust your Capfile to look like this:'
-puts ''
+puts
 puts "require 'rubygems'"
 puts "require 'wordpress'"
 puts "load 'deploy'"
 puts "load 'deploy/wordpress'"
 puts "load 'config/deploy'"
-puts ''
+puts
 
-load 'deploy'
-load 'deploy/wordpress'
+Capistrano::Configuration.instance(:must_exist).load do
+  load 'deploy'
+  load 'deploy/wordpress'
+end
